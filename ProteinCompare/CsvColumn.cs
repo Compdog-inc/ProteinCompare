9727207129417dc6d86de06bcde88fe6cd72becd
Compdog Inc <compdog.inc@gmail.com>
2023-07-26 02:24:13 -0400

    Add csv column class

---
+++ ProteinCompare/CsvColumn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProteinCompare
+{
+    public class CsvColumn
+    {
+        public string? Name { get; set; }
+        public CsvType Type { get; set; }
+
+        public CsvColumn(CsvType type)
+        {
+            Type = type;
+        }
+
+        public CsvColumn(string name, CsvType type)
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+}
