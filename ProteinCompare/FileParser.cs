0e2503e4c91ea10ba7956a774043b428fd9a6844
Compdog Inc <compdog.inc@gmail.com>
2023-07-25 23:26:02 -0400

    Add project files.

---
+++ ProteinCompare/FileParser.cs
@@ -0,0 +1,79 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProteinCompare
+{
+    public static class FileParser
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        private static IEnumerable<string> _parseSegment(string path, string[] parts)
+        {
+            logger.Trace("Segment {path} / [{parts}]", path, parts);
+            if (parts.Length == 0) return new[] { path };
+
+            if (!parts[0].Contains('*'))
+            {
+                logger.Trace("Fully defined part");
+                var basePath = Path.GetFullPath(parts[0] + '/', path).Replace('\\', '/');
+                return _parseSegment(basePath, parts[1..]);
+            }
+            else if (!parts[0].Contains('.'))
+            {
+                logger.Trace("Partially definined directory");
+                IEnumerable<string> paths = new List<string>();
+                // relative base
+                foreach (var basePath in Directory.GetDirectories(path, parts[0]))
+                {
+                    paths = paths.Concat(_parseSegment(basePath.Replace('\\', '/'), parts[1..]));
+                }
+                return paths;
+            } else if (!string.IsNullOrEmpty(parts[0]))
+            {
+                logger.Trace("Partially definined file");
+                IEnumerable<string> paths = new List<string>();
+                // relative base
+                foreach (var basePath in Directory.GetFiles(path, parts[0], SearchOption.TopDirectoryOnly))
+                {
+                    paths = paths.Concat(_parseSegment(basePath.Replace('\\', '/'), parts[1..]));
+                }
+                return paths;
+            } else
+            {
+                logger.Trace("Unknown directory tree");
+                IEnumerable<string> paths = new List<string>();
+
+                // peek next path
+                if (parts.Length > 1)
+                {
+
+                }
+
+                // relative base
+                foreach (var basePath in Directory.GetDirectories(path))
+                {
+                    // keep searching
+                    paths = paths.Concat(_parseSegment(basePath.Replace('\\', '/'), parts));
+                }
+                return paths;
+            }
+        }
+
+        public static IEnumerable<string> Parse(string path)
+        {
+            logger.Trace("Parsing {path}", path);
+            string[] parts = path.Replace('\\', '/').Split('/');
+            return _parseSegment("", parts);
+        }
+
+        public static IEnumerable<string> Parse(IEnumerable<string> paths)
+        {
+            return paths.SelectMany(Parse);
+        }
+    }
+}
---
+++ ProteinCompare/ProteinCompare.cs
@@ -0,0 +1,72 @@
+﻿using CommandLine;
+using NLog;
+using NLog.Layouts;
+using NLog.Targets;
+
+namespace ProteinCompare
+{
+    class ProteinCompare
+    {
+        public class Options
+        {
+            [Option('v', "verbose", Required = false, HelpText = "Enable verbose messages.", Default = false)]
+            public bool Verbose { get; set; }
+
+            [Value(0, MetaName = "[...files]", MetaValue = "<string>", HelpText = "List of paths to protein files (separated by spaces)")]
+            public IEnumerable<string>? Files { get; set; }
+        }
+
+        public static int Main(string[] args)
+        {
+            return new Parser(p =>
+            {
+                p.EnableDashDash = true;
+                p.AutoHelp = true;
+                p.HelpWriter = Console.Error;
+            }).ParseArguments<Options>(args)
+            .MapResult(
+              options => new ProteinCompare(options).Start(),
+              _ => 1);
+        }
+
+        private Options options;
+
+        public ProteinCompare(Options options)
+        {
+            this.options = options;
+            LogManager.Setup().LoadConfiguration(builder =>
+            {
+                // StdOut for <= Warn
+                builder.ForLogger().FilterLevels(options.Verbose ? LogLevel.Trace : LogLevel.Info, LogLevel.Warn).WriteTo(new ColoredConsoleTarget()
+                {
+                    StdErr = false,
+                    AutoFlush = true,
+                    Layout = Layout.FromMethod(evt =>
+                {
+                    return string.Format("{0}|{1}: {2}", evt.Level.Name.ToUpperInvariant(), evt.LoggerName, evt.FormattedMessage);
+                })
+                });
+
+                // StdErr for >= Error
+                builder.ForLogger().FilterMinLevel(LogLevel.Error).WriteTo(new ColoredConsoleTarget()
+                {
+                    StdErr = true,
+                    AutoFlush = true,
+                    Layout = Layout.FromMethod(evt =>
+                    {
+                        return string.Format("{0}|{1}: {2}", evt.Level.Name.ToUpperInvariant(), evt.LoggerName, evt.FormattedMessage);
+                    })
+                });
+            });
+        }
+
+        public int Start()
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+
+            logger.Trace(FileParser.Parse(options.Files!).Aggregate((a, b) => a + ", " + b));
+
+            return 0;
+        }
+    }
+}
