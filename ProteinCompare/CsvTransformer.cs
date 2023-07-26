0cd61c1177e7bc23ac15850da7740d21726f6e7d
Compdog Inc <compdog.inc@gmail.com>
2023-07-26 02:06:32 -0400

    Added file and row transformer

---
+++ ProteinCompare/CsvTransformer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProteinCompare
+{
+    public static class CsvTransformer
+    {
+        public static string[] TransformText(string text, char rowDelimiter)
+        {
+            return text.Split(rowDelimiter);
+        }
+
+        public static string[] TransformRow(string row, CsvDialect dialect)
+        {
+            var columns = row.Split(dialect.Delimiter);
+
+            // remove quotes
+            if (dialect.Quote != null)
+            {
+                for (int i = 0; i < columns.Length; i++)
+                {
+                    columns[i] = columns[i][1..^2];
+                }
+            }
+
+            // remove escape
+            if (dialect.Escape != null)
+            {
+                for (int i = 0; i < columns.Length; i++)
+                {
+                    StringBuilder sb = new();
+
+                    for (int j = 0; j < columns[i].Length; j++)
+                    {
+                        if (columns[i][j] == dialect.Escape && j < columns[i].Length - 1)
+                        {
+                            // detect escaped char and skip escape code
+                            switch (columns[i][j + 1])
+                            {
+                                case 't':
+                                    sb.Append('\t');
+                                    j++;
+                                    break;
+                                case 'b':
+                                    sb.Append('\b');
+                                    j++;
+                                    break;
+                                case 'n':
+                                    sb.Append('\n');
+                                    j++;
+                                    break;
+                                case 'r':
+                                    sb.Append('\r');
+                                    j++;
+                                    break;
+                                case 'f':
+                                    sb.Append('\f');
+                                    j++;
+                                    break;
+                                case 's':
+                                    sb.Append(' ');
+                                    j++;
+                                    break;
+                                case '\'':
+                                    sb.Append('\'');
+                                    j++;
+                                    break;
+                                case '"':
+                                    sb.Append('"');
+                                    j++;
+                                    break;
+                                case '\\':
+                                    sb.Append('\\');
+                                    j++;
+                                    break;
+                                default:
+                                    if (columns[i][j + 1] == dialect.Quote && dialect.Quote != null)
+                                    {
+                                        // quote escaped
+                                        sb.Append(dialect.Quote);
+                                        j++;
+                                    }
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            sb.Append(columns[i][j]);
+                        }
+                    }
+
+                    columns[i] = sb.ToString();
+                }
+            }
+
+            return columns;
+        }
+    }
+}
